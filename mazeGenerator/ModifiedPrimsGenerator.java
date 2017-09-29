59bd213419a31812d8fd4ca308c2f7615191cd27
rmit-s3586372-rahul-raghavan <s3586372@student.rmit.edu.au>
2017-09-29 11:19:08 +1000

    Modified prims generator

--- mazeGenerator/ModifiedPrimsGenerator.java
+++ mazeGenerator/ModifiedPrimsGenerator.java
@@ -4,9 +4,29 @@ import maze.Maze;
 
 public class ModifiedPrimsGenerator implements MazeGenerator {
 
+	public ArrayList<Cell> getNeighbours(Maze maze, Cell cell) {
+		ArrayList<Cell> neighbours = new ArrayList<>();
+		for(int i =0; i < maze.sizeR; i++) {
+			for (int j=0; j<maze.NUM_DIR; j++) {
+				Cell neigh = maze.map[i + maze.deltaR[k]][i + maze.deltaC[k]];
+				neighbours.add();
+			}
+			return neighbours;
+		}
+	}
+
 	@Override
 	public void generateMaze(Maze maze) {
 		// TODO Auto-generated method stub
+
+		// Create visited set
+		ArrayList<Cell> visited = new ArrayList<>();
+		// Create frontier set
+		ArrayList<Cell> frontier = new ArrayList<>();
+
+		Cell entrance = maze.entrance;
+		ArrayList<Cell> neighbours = getNeighbours(maze, entrance);
+		visited.add(entrance);
 
 	} // end of generateMaze()
 
