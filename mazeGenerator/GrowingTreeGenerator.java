05a849384506030951f91f7db8aba929528ff3c6
rmit-s3586372-rahul-raghavan <s3586372@student.rmit.edu.au>
2017-10-16 23:03:26 +1100

    Added random selection of 2 strategies for growingtree alfgorithm - GrowingTree now selects between choosing the last added cell in the   travel set and randomly selecting a cell from the travel set specified   by the threshold value. - Added comments for GrowingTree generator

--- mazeGenerator/GrowingTreeGenerator.java
+++ mazeGenerator/GrowingTreeGenerator.java
@@ -6,33 +6,71 @@ import maze.Maze;
 import maze.Cell;
 import static maze.Maze.oppoDir;
 
+/**
+ * Generate a maze using a growing tree generator using a mixture of two different
+ * strategies used to select a cell to pick from the travel set. The ratio is specified
+ * by the threshold value.
+ * 
+ * Strategy 1 involves selecting the newest cell added to the travel set.
+ * Strategy 2 involves selecting a random cell from the travel set.
+ * 
+ * @author Huligan
+ */
 public class GrowingTreeGenerator implements MazeGenerator {
 	// Growing tree maze generator. As it is very general, here we implement as "usually pick the most recent cell, but occasionally pick a random cell"
 	
 	double threshold = 0.1;
 	
+        /**
+         * Method to generate the maze using the mixture of strategies for growing tree
+         * @param maze 
+         */
 	@Override
 	public void generateMaze(Maze maze) {
+            // Get the maze entrance
             Cell currCell = maze.entrance;
             
-            // setup travel cells
+            /**
+             * Setup the travel cells - which is the set from which the cells to 
+             * travel to next are picked.
+             */
             Stack<Cell> travelCells = new Stack<>();
             HashSet<Cell> visitedCells = new HashSet<>();
+            
+            // Add the entrance to the travel set, and set this as the last added cell
             Cell lastAddedCell = currCell;
             travelCells.push(currCell);
-             
+            
+            /**
+             * Keep selecting a cell from the travel set using either of the two
+             * strategies until the set is empty
+             */
             while(!travelCells.isEmpty()) {
                 // Mark the current cell as visited
                 maze.drawFtPrt(currCell);
                 visitedCells.add(currCell);
-                System.out.println("r:" + currCell.r + " c:"+currCell.c);
-                System.out.println(travelCells.size());
-                // Pick a cell from the travel set to visit
-                currCell = travelCells.pop();
+
+                /** 
+                 * Treat the threshold as a probability
+                 * Select a random cell from the travel set at a 
+                 * probability specified by the threshold.
+                 * 
+                 * In all other cases, select the latest cell added to the 
+                 * travel set.
+                 */
+                // If the probability falls within the threshold probability - 
+                if(new java.util.Random().nextInt((int)(threshold*100.00)) == 0) {
+                    // Select a random cell from the travel set
+                    currCell = travelCells.get(new java.util.Random().nextInt(travelCells.size()));
+                    travelCells.remove(currCell);                    
+                }
+                else {
+                    // Otherwise pick a cell from the travel set to visit
+                    currCell = travelCells.pop();  
+                }
                 
                 // Iterate through all neighbours of the selected cell
                 HashMap<Cell, Integer> neighbours = new HashMap<>();
-                boolean carved = false;
                 for (int i=0; i<currCell.neigh.length; i++) {
                     // Check that the neighbour is valid and exists
                     if(currCell.neigh[i] != null) {
@@ -69,7 +107,6 @@ public class GrowingTreeGenerator implements MazeGenerator {
                 else {
                     // All neighbours have been visited for this cell
                     // Remove cell from the travel set
-                    System.out.println("reoved");
                     travelCells.remove(currCell);
                 }
             }
